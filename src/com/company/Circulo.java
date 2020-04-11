be76391dde03d5002cc6ee370b95a95a4d5c06ad
Octavio <octavioblanc234@gmail.com>
2020-04-11 14:21:44 -0300

    Circulo codeado

--- src/com/company/Circulo.java
+++ src/com/company/Circulo.java
@@ -1,4 +1,21 @@
 package com.company;
 
 public class Circulo {
+    protected float radio;
+    protected String color;
+
+    public  Circulo(){
+        radio =  (float)1.0;
+        color = "Rojo";
+    }
+
+    public float calcularArea(){
+        return (float)3.14*(radio*radio);
+    }
+
+    public void mostrarDatos(){
+        System.out.println("Color: "+ color);
+        System.out.println("Radio: "+ radio);
+        System.out.println("Area: "+ calcularArea());
+    }
 }
